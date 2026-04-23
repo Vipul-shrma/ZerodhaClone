994bf9035cc0b6192d91a4f09f5c5e6f029aac61
Vipul-shrma <vipulsharma2376@gmail.com>
2026-04-23 14:34:38 +0530

    Initial commit - Zerodha Clone by Vipul

---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+function Home() {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://zerodhabackend-2cdq.onrender.com/userAuthenticate", { withCredentials: true })
+      .then((res) => {
+        if (res?.data?.authenticated) {
+          setUserData(res.data.user);
+        } else {
+          alert("you aren't logged in.")
+          setTimeout(() => {
+            window.location.href = "https://zerodhatrading.onrender.com";
+          }, 1000);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert("Something Went Wrong !")
+        setTimeout(() => {
+          window.location.href = "https://zerodhatrading.onrender.com";
+        }, 2000);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {userData ? (
+        <div>
+          <TopBar userData={userData} />
+          <Dashboard userData={userData} />
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/src/landing_page/Signup/Signup.jsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import HomePage from "../Home/HomePage";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+function Signup() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  useEffect(() => {
+    axios
+      .get("https://zerodhabackend-2cdq.onrender.com/userAuthenticate", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res?.data?.authenticated) {
+          // Already logged in, redirect to dashboard
+          toast.success("You've already logged in redirecting .");
+          setisLoggedIn(true);
+          setTimeout(() => {
+            window.location.href =
+              "https://dashboard-zerodhatrading.onrender.com";
+          }, 4000);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setLoader(true);
+    try {
+      const res = await axios.post(
+        "https://zerodhabackend-2cdq.onrender.com/signup",
+        formData
+      );
+      setLoader(false);
+      if (res?.data?.user != undefined) {
+        toast.success("Signup successfull ! Redirecting to Login page..");
+        setisLoggedIn(true);
+        setTimeout(() => {
+          window.location.href = "https://zerodhatrading.onrender.com/login";
+        }, 3000);
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      {isLoggedIn ? (
+        <HomePage />
+      ) : (
+        <section>
+          <div className="row my-4">
+            <div className="col-sm-12 text-center p-5">
+              <h1 className="fs-1">
+                Open a free demat and trading account online
+              </h1>
+              <p className="fs-4">
+                Start investing brokerage free and join a community of 1.5+
+                crore investors and traders
+              </p>
+            </div>
+          </div>
+          <div className="row p-3">
+            <div className="col-sm-6">
+              <img
+                src="img/signup.svg"
+                className="img-fluid"
+                alt="signup-image"
+              />
+            </div>
+            <div className="col-sm-6">
+              {loader ? (
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <CircularProgress />
+                </Box>
+              ) : null}
+              <div>
+                <h2>Signup now</h2>
+                <p className="fs-5">On track your existing application</p>
+              </div>
+              <form onSubmit={handleFormSubmit}>
+                <div className="input-group mb-3">
+                  <span className="input-group-text bg-white" id="basic-addon1">
+                    @
+                  </span>
+                  <input
+                    type="text"
+                    className="form-control p-3"
+                    placeholder="Enter your Username"
+                    aria-label="username"
+                    aria-describedby="basic-addon1"
+                    name="username"
+                    onChange={(e) => {
+                      setFormData({ ...formData, username: e.target.value });
+                    }}
+                    required
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="email"
+                    className="form-control p-3"
+                    placeholder="Enter your Email id"
+                    aria-label="email"
+                    aria-describedby="basic-addon1"
+                    name="email"
+                    onChange={(e) => {
+                      setFormData({ ...formData, email: e.target.value });
+                    }}
+                    required
+                  />
+                </div>
+                <div className="input-group mb-3">
+                  <input
+                    type="password"
+                    className="form-control p-3"
+                    placeholder="Create a Password"
+                    aria-label="password"
+                    aria-describedby="basic-addon1"
+                    name="password"
+                    onChange={(e) => {
+                      setFormData({ ...formData, password: e.target.value });
+                    }}
+                    required
+                  />
+                </div>
+                <button className="btn btn-primary p-3 w-100">Sign up</button>
+              </form>
+              <Link to="/login" className="nav-link mt-2 text-center">
+                Already have an account?Login
+              </Link>
+
+              <p className="mt-2">
+                By proceeding, you agree to the Zerodha terms & privacy policy
+              </p>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
+
+export default Signup;
